b22142492b98bf7abaeabb55a69f331272942f23
Ehsan Kazemi <ehsankazemi@Ehsans-MacBook-Pro.local>
2026-05-31 18:34:56 -0700

    add ph-pages

--- marine_sim/__init__.py
+++
@@ -1,8 +0,0 @@
-from gym.envs.registration import register
-
-USV_NAVIGATION_ENV_ID = "USVNavigation-v0"
-
-register(
-    id=USV_NAVIGATION_ENV_ID,
-    entry_point="marine_sim.envs:MarineNavigationEnv",
-)
--- marine_sim/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from .marine_env import MarineNavigationEnv
